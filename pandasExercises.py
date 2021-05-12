55dd540fad3407b7a3cf5b40f7607df6aa227308
Aaman Lamba <aamanlamba@gmail.com>
2021-05-12 09:07:12 -0400

    18

--- pandasExercises.py
+++ pandasExercises.py
@@ -40,3 +40,67 @@ ser4 = pd.Series(np.random.normal(10,5,25))
 df4 = pd.DataFrame(ser4)
 print(df4.describe())
 print(np.percentile(ser4,q=[0,25,50,75,100]))
+
+#get frequency counts of unique items of a series
+ser = pd.Series(np.take(list('abcdefgh'), np.random.randint(8, size=30)))
+print(ser)
+uniq = ser.unique()
+print(uniq)
+print(ser.value_counts())
+
+#keep only 2 most frequent values
+val = ser.value_counts()
+ser[~ser.isin(ser.value_counts().index[:2])] = 'Other'
+print(ser.value_counts())
+#split series into deciles
+ser2 = pd.Series(np.random.random(20))
+
+print(pd.qcut(ser2,q=[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1],
+labels=['1st','2nd','3rd','4th','5th','6th','7th','8th','9th','10th']))
+
+#reshape series into dataframe
+ser3 = pd.Series(np.random.randint(1,10,35))
+df3 = pd.DataFrame(ser3.values.reshape(7,5))
+print(df3)
+
+#find positions of numbers that are multiples of 3
+ser4 = pd.Series(np.random.randint(1,10,7))
+#print(np.argwhere(ser4 % 3))
+mylist = list('abcdefghijklmnopqrstuvwxyz')
+pSeries1 = pd.Series(mylist)
+pos = [0,4,8,14,20]
+
+for idx in pos:
+    print(pSeries1[idx])
+
+ser1 = pd.Series(range(5))
+print(ser1)
+ser2 = pd.Series(list('abcde'))
+ser1.append(ser2)
+print(ser1)
+print(ser2)
+df12 = pd.concat([ser1,ser2],axis = 1)
+print(df12)
+
+#get position of elements from another series
+ser1 = pd.Series([10,9,6,5,3,1,12,8,13])
+ser2 = pd.Series([1,3,10,13])
+print([np.where(i == ser1)[0].tolist()[0] for i in ser2])
+print([pd.Index(ser1).get_loc(el) for el in ser2])
+
+#compute mean squared error
+truth = pd.Series(range(10))
+pred = pd.Series(range(10)) + np.random.random(10)
+print("truth",truth)
+print("pred",pred)
+err = pow((pred - truth),2)
+mse = 1/len(truth) * sum(err)
+mse2 = np.mean(err)
+print("err",err)
+print("mse",mse)
+print("mse2",mse2)
+
+#convert elements
+ser = pd.Series(['how','to','do','this'])
+print(ser.map(lambda x: x.title()))
+print(pd.Series([i.title() for i in ser]))
